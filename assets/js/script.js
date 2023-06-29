543aaa3ed536aa9c0aa2eee9509fd5d7f786e499
Marketing Director <nicolestark1103@gmail.com>
2023-06-29 11:06:35 -0500

    javascript finished local storage works but want to see if I can make it into a loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,17 +36,24 @@ $(document).ready(function () {
                     $(this).removeClass("present");
                     $(this).addClass("future");
                 }
-            })
-        }
+            });
+        };
 
-        
-
-        
         // TODO: Add code to get any user input that was saved in localStorage and set
         // the values of the corresponding textarea elements. HINT: How can the id
         // attribute of each time-block be used to do this?
         //
-        // TODO: Add code to display the current date in the header of the page.
+        
+        // Get saved entries if any
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
     // Run trackTime function
